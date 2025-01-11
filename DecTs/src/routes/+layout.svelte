65178e678c137b7fca6807c7c84932d6127daa4e
Jake <jh0231015@otc.edu>
2025-01-11 01:15:28 -0600

    Navbar done....creating wireframe instead of wasting time on css

---
+++ DecTs/src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+</script>
+
+<header>
+    <div class="Navbar">
+        <a href="/"><img src="/DeckT.png" alt="deckt imgage" /></a>
+        <ul>
+            <li><a href="/collections">Collections</a></li>
+            <li><a href="/create">Create</a></li>
+            <li><a href="/TheWord">The-Word</a></li>
+        </ul>
+    </div>
+</header>
+
+
+<main>
+    <slot/>
+</main>
--- DecTs/src/routes/+page.svelte
+++ DecTs/src/routes/+page.svelte
@@ -2,6 +2,14 @@
 
 </script>
 
-<div>
-    <h1 class="intro">Hello again, it's me!</h1>
+<div class="intro">
+    <h1>Don't Invest.</h1> <h1>Collect.</h1>
+    <p>  I don't believe crypto was created to simply make money like the bulls and bullshiters of wallstreet. I believe it was created to show a new path. To give worth to the bits and bytes of our new environment.
+        While I enjoy making money like everyone else it's hard not to notice the greed, lies, and bullshit of this space. 
+    </p>
+<br>
+<br>
+    <p>Here we want to do something different. We want a place where you can create something of value, with scarcity built in. This isn't another Faze Clan pump and dump webapp. This is where everyone can express themselves and pushout projects that will be sought out
+        for generations. 
+    </p>
 </div>
